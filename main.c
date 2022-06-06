f243959255232b16d1c930672957b9036f9d9c34
CaokimHanh <106530492+CaokimHanh@users.noreply.github.com>
2022-06-06 15:29:43 +0700

    Create main.c

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	char letter[30];
+	printf ("nhap vao letter:");
+	scanf ("%s", letter);
+	printf ("letter is: %s", letter);
+
+	return 0;
+}
